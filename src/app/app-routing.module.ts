9d04c47f7882cb495d9a6f89945ccd7cb5b7a203
Gupta, Sunny <sunnyvales789@gmail.com>
2024-12-15 11:38:41 +0530

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TechComponent } from './tech/tech.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path:'', component: TechComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
